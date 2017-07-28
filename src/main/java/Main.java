44a0c50f86ec26a432de09d65a6903f6ed632cb7
agliullin <ratmir-91@mail.ru>
2017-07-28 10:46:50 +0300

    functional interfaces and lambda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,34 +7,49 @@ public class Main {
 
 
 
+
     @FunctionalInterface
-    interface functionalInterface{
-        abstract public double abstractMethod(double x);
+    interface GetName{
+        public String get(Team team);
+    }
+    @FunctionalInterface
+    interface GetPoints{
+        public int get(Team team);
     }
 
-    static class Logarifm implements functionalInterface{
+//    static class Logarifm{
+//
+//
+//        private static void print(double x, Get get){
+//
+//            System.out.println(Math.log(get.get(x)));
+//        }
+//    }
 
-        @Override
-        public double abstractMethod(double x) {
-            return Math.log(x);
-        }
+
+    public static void printTeamName(Team team, GetName gn){
+        System.out.println(gn.get(team));
     }
     public static void main(String[] args)  {
+Team real = new Team("real madrid",50);
+Team barca = new Team("barcelona", 45);
+printTeamName(real, c->c.getName());
+//
+//
+//     Set<Double> doubleSet = new HashSet<>();
+//     doubleSet.add(1D);
+//     doubleSet.add(0.2D);
+//        Set<Double> newSet = new HashSet<>();
+//              doubleSet.stream().forEach(x->{
+//
+//         x= Math.random();
+//        newSet.add(x);
+//         System.out.println(x);}
+//     );
+//        System.out.println(newSet.stream().filter(x->x>0.5).count());
+//
 
-        Logarifm logarifm = new Logarifm();
-        
 
-     Set<Double> doubleSet = new HashSet<>();
-     doubleSet.add(1D);
-     doubleSet.add(0.2D);
-        Set<Double> newSet = new HashSet<>();
-              doubleSet.stream().forEach(x->{
-
-         x= Math.random();
-        newSet.add(x);
-         System.out.println(x);}
-     );
-        System.out.println(newSet.stream().filter(x->x>0.5).count());
 
     }
 }
---
+++ src/main/java/Team.java
@@ -0,0 +1,25 @@
+public class Team {
+    private String name;
+    private int points;
+
+    public Team(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
